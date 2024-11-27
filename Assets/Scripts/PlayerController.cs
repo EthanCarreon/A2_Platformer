ab1ba63d3185ef2e4d84844ecdf7196b5fa5e4e1
Nqla <151285370+EthanCarreon@users.noreply.github.com>
2024-11-26 22:52:01 -0500

    code didnt commit properly??
    what the shit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,10 +10,24 @@ public class PlayerController : MonoBehaviour
     float acceleration;
     Vector3 velocity;
 
+    public float apexHeight = 2f;
+    public float apexTime = 3f;
+    public float coyoteTime;
+    public float maxCoyoteTime = 1f;
+
+    float jump;
+    float gravity;
+
+    public float jumpVelocity;
+
     Rigidbody2D rb;
 
     public LayerMask ground;
     public Transform player;
+
+    public bool jumped = false;
+
+    public float terminalSpeed = -5f;
 
     public enum FacingDirection
     {
@@ -25,20 +39,42 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        gravity = (-2 * apexHeight) / (Mathf.Pow(apexTime, 2));
+        jump = (2 * apexHeight) / apexTime;
+
+        acceleration = maxSpeed / accelerationTime;
+
+        coyoteTime = maxCoyoteTime;
     }
 
     void Update()
     {
+        if ((Input.GetKeyDown(KeyCode.Space) && (IsGrounded() || coyoteTime > 0)) && !jumped)
+        {
+            jumped = true;
+            jumpVelocity = jump;
+        }
+
+        if (coyoteTime <= 0)
+        {
+            coyoteTime = 0;
+        }
+
+        Debug.Log(IsGrounded());
+    }
+
+    void FixedUpdate()
+    {
         //The input from the player needs to be determined and then passed in the to the MovementUpdate which should
         //manage the actual movement of the character.
-        Vector2 playerInput = new Vector2(Input.GetAxis("Horizontal"), 0).normalized;
+        Vector2 playerInput = new Vector2(Input.GetAxis("Horizontal"), 0);
         MovementUpdate(playerInput);
+
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
-        acceleration = maxSpeed / accelerationTime;
-
         if (playerInput.magnitude > 0)
         {
             currentSpeed += acceleration * Time.deltaTime;
@@ -48,7 +84,7 @@ public class PlayerController : MonoBehaviour
                 currentSpeed = maxSpeed;
             }
 
-            velocity = playerInput;
+            velocity = playerInput * currentSpeed;
         }
 
         else
@@ -60,13 +96,56 @@ public class PlayerController : MonoBehaviour
             {
                 currentSpeed = 0;
             }
+
+            velocity = Vector2.zero;
         }
 
-        transform.position += velocity * currentSpeed * Time.deltaTime;
+        rb.velocity = new Vector2(velocity.x, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        if (!IsGrounded())
         {
-            rb.velocity = new Vector2(rb.velocity.x, 5f);
+            coyoteTime -= Time.deltaTime;
+
+        }
+        else
+        {
+            coyoteTime = maxCoyoteTime;
+        }
+
+
+        if (jumped)
+        {
+            jumpVelocity += gravity * Time.deltaTime;
+            rb.velocity = new Vector2(rb.velocity.x, jumpVelocity);
+
+            if (jumpVelocity < terminalSpeed)
+            {
+                jumpVelocity = terminalSpeed;
+            }
+
+            // when jump velocity reaches its max point, jump velocity increases, and then retracts because gravity subtracts it
+            // the reason why it has to be ZERO is because the jump velocity first gets added from the jump formula, and then
+            // gravity will also be applied at the same time, and once they meet the highest point, the jump velocity will become equal to or less than 0
+            // that is when the jumped boolean should be set to false (it is technically no longer jumping, and gravity is just doing the rest)
+
+            if (jumpVelocity <= 0)
+            {
+                jumped = false;
+            }
+        }
+
+        else if (!IsGrounded())
+        {
+            // applies gravity when player is no longer grounded
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y + gravity * Time.deltaTime, terminalSpeed);
+        }
+
+        else
+        {
+            if (IsGrounded() && rb.velocity.y < 0f)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, 0);
+            }
         }
     }
 
@@ -80,12 +159,14 @@ public class PlayerController : MonoBehaviour
         {
             return false;
         }
-        
+
     }
     public bool IsGrounded()
     {
-        bool checkGrounded = Physics2D.Raycast(player.position, Vector2.down, 1f, ground);
-        Debug.DrawRay(player.position, Vector2.down * 1f, Color.green);
+        Vector2 direction = new Vector2(1, -1);
+
+        bool checkGrounded = Physics2D.Raycast(player.position, Vector2.down, 0.65f, ground);
+        Debug.DrawRay(player.position, Vector2.down * 0.65f, Color.green);
 
         return checkGrounded;
     }
@@ -104,5 +185,5 @@ public class PlayerController : MonoBehaviour
         }
         return currentDirection;
     }
-    
+
 }
